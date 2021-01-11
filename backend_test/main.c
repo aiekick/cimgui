77bb6827495fba2c810996887ca61ab915c1ea6a
Aiekick <aiekick@funparadigm.com>
2021-01-11 21:57:49 +0100

    -

--- backend_test/main.c
+++ backend_test/main.c
@@ -103,7 +103,7 @@ int main(int argc, char* argv[])
     //IGFD_SetExtentionInfos2(cfiledialog, ".c", 1.0f, 1.0f, 0.0f, 0.9f, "");
     //IGFD_SetExtentionInfos2(cfiledialog, ".h", 0.2f, 1.0f, 0.0f, 0.9f, "");
 
-    IGFD_String res = IGFD_String_Get();
+    struct IGFD_String res = IGFD_String_Get();
 
     bool showDemoWindow = true;
     bool showAnotherWindow = false;
