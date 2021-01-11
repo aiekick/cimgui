05bf608e3696d8b6447049e5950aa483d161822a
Aiekick <aiekick@funparadigm.com>
2021-01-11 21:51:50 +0100

    [ADD] : add imguifiledialog for c interface

---
+++ backend_test/CustomFont.h
@@ -0,0 +1,67 @@
+//Header Generated with https://github.com/aiekick/ImGuiFontStudio
+//Based on https://github.com/juliettef/IconFontCppHeaders
+//for Language c
+
+#ifndef IGFD_FONT_ICON_H
+#define IGFD_FONT_ICON_H
+
+#define FONT_ICON_BUFFER_NAME_IGFD compressed_data_base85
+#define FONT_ICON_BUFFER_SIZE_IGFD 0x27a1
+
+#define ICON_IGFD_MIN 0xf002
+#define ICON_IGFD_MAX 0xf26c
+
+#define ICON_IGFD_ABOUT u8"\uf129"
+#define ICON_IGFD_ADD u8"\uf067"
+#define ICON_IGFD_BARCODE u8"\uf02a"
+#define ICON_IGFD_BOOKMARK u8"\uf02e"
+#define ICON_IGFD_BY_FONT u8"\uf031"
+#define ICON_IGFD_CANCEL u8"\uf00d"
+#define ICON_IGFD_COLUMNS u8"\uf0db"
+#define ICON_IGFD_COPY u8"\uf0c5"
+#define ICON_IGFD_CUT u8"\uf0c4"
+#define ICON_IGFD_DESTROY u8"\uf014"
+#define ICON_IGFD_DRIVES u8"\uf233"
+#define ICON_IGFD_EDIT u8"\uf040"
+#define ICON_IGFD_ERROR u8"\uf057"
+#define ICON_IGFD_EXCLAMATION u8"\uf12a"
+#define ICON_IGFD_FILE u8"\uf016"
+#define ICON_IGFD_FILE_TYPE_PROJECT u8"\uf1c4"
+#define ICON_IGFD_FILE_TYPE_TEXT u8"\uf0f6"
+#define ICON_IGFD_FILTER u8"\uf0b0"
+#define ICON_IGFD_FOLDER u8"\uf114"
+#define ICON_IGFD_FOLDER_OPEN u8"\uf115"
+#define ICON_IGFD_FULLSCREEN u8"\uf0b2"
+#define ICON_IGFD_GENERATE u8"\uf085"
+#define ICON_IGFD_GLYPHS u8"\uf003"
+#define ICON_IGFD_HELP u8"\uf128"
+#define ICON_IGFD_ICON_EDIT u8"\uf14b"
+#define ICON_IGFD_ICON_VIEW u8"\uf0c8"
+#define ICON_IGFD_INFOS u8"\uf05a"
+#define ICON_IGFD_LAYOUT u8"\uf26c"
+#define ICON_IGFD_LINK u8"\uf1c9"
+#define ICON_IGFD_LIST u8"\uf03a"
+#define ICON_IGFD_MENU_LEFT u8"\uf0d9"
+#define ICON_IGFD_MENU_RIGHT u8"\uf0da"
+#define ICON_IGFD_MENU_UP u8"\uf0d8"
+#define ICON_IGFD_OK u8"\uf00c"
+#define ICON_IGFD_PASTE u8"\uf0ea"
+#define ICON_IGFD_PLAY_CIRCLE u8"\uf01d"
+#define ICON_IGFD_PROJECT u8"\uf02d"
+#define ICON_IGFD_REDO u8"\uf01e"
+#define ICON_IGFD_REFRESH u8"\uf021"
+#define ICON_IGFD_REMOVE u8"\uf068"
+#define ICON_IGFD_RESET u8"\uf064"
+#define ICON_IGFD_SAVE u8"\uf0c7"
+#define ICON_IGFD_SEARCH u8"\uf002"
+#define ICON_IGFD_SELECT u8"\uf245"
+#define ICON_IGFD_SELECT_PATH u8"\uf0d0"
+#define ICON_IGFD_SETTINGS u8"\uf1de"
+#define ICON_IGFD_SORT_ASC u8"\uf077"
+#define ICON_IGFD_SORT_DESC u8"\uf078"
+#define ICON_IGFD_TARGET u8"\uf05b"
+#define ICON_IGFD_TEXTURE u8"\uf03e"
+#define ICON_IGFD_UNDO u8"\uf0e2"
+#define ICON_IGFD_WARNING u8"\uf071"
+
+#endif // IGFD_FONT_ICON_H
--- backend_test/main.c
+++ backend_test/main.c
@@ -2,6 +2,8 @@
 #include "cimgui.h"
 #include "cimgui_extras.h"
 #include "cimgui_impl.h"
+#include "ImGuiFileDialog.h"
+
 #include <stdio.h>
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
@@ -11,19 +13,17 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 
-
-
-SDL_Window *window = NULL;
+SDL_Window* window = NULL;
 
 int main(int argc, char* argv[])
 {
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    SDL_Log("failed to init: %s", SDL_GetError());
-    return -1;
-  }
- 
-  // Decide GL+GLSL versions
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("failed to init: %s", SDL_GetError());
+        return -1;
+    }
+
+    // Decide GL+GLSL versions
 #if __APPLE__
     // GL 3.2 Core + GLSL 150
     const char* glsl_version = "#version 150";
@@ -40,158 +40,233 @@ int main(int argc, char* argv[])
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #endif
 
-  // and prepare OpenGL stuff
-  SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-  SDL_DisplayMode current;
-  SDL_GetCurrentDisplayMode(0, &current);
-  
-  window = SDL_CreateWindow(
-      "Hello", 0, 0, 1024, 768,
-      SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
-      );
-  if (window == NULL) {
-    SDL_Log("Failed to create window: %s", SDL_GetError());
-    return -1;
-  }
+    // and prepare OpenGL stuff
+    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_DisplayMode current;
+    SDL_GetCurrentDisplayMode(0, &current);
 
-  SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-  SDL_GL_SetSwapInterval(1);  // enable vsync
+    window = SDL_CreateWindow(
+        "Hello", 0, 0, 1024, 768,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+    if (window == NULL) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return -1;
+    }
 
-  // Initialize OpenGL loader for cimgui_sdl
-  bool err = Do_gl3wInit() != 0;
-  if (err)
-  {
-    SDL_Log("Failed to initialize OpenGL loader for cimgui_sdl!");
-    return 1;
-  }
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_SetSwapInterval(1);  // enable vsync
 
-  // check opengl version sdl uses
-  SDL_Log("opengl version: %s", (char*)glGetString(GL_VERSION));
+    // Initialize OpenGL loader for cimgui_sdl
+    bool err = Do_gl3wInit() != 0;
+    if (err)
+    {
+        SDL_Log("Failed to initialize OpenGL loader for cimgui_sdl!");
+        return 1;
+    }
 
-  // setup imgui
-  igCreateContext(NULL);
-  
-  //set docking
-  ImGuiIO* ioptr = igGetIO();
-  ioptr->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-  //ioptr->ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    // check opengl version sdl uses
+    SDL_Log("opengl version: %s", (char*)glGetString(GL_VERSION));
+
+    // setup imgui
+    igCreateContext(NULL);
+
+    //set docking
+    ImGuiIO* ioptr = igGetIO();
+    ioptr->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+    //ioptr->ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 #ifdef IMGUI_HAS_DOCK
-  ioptr->ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-  ioptr->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+    ioptr->ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    ioptr->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 #endif
-  
-  ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-  ImGui_ImplOpenGL3_Init(glsl_version);
 
-  igStyleColorsDark(NULL);
-  //ImFontAtlas_AddFontDefault(io.Fonts, NULL);
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
 
+    igStyleColorsDark(NULL);
 
-  bool showDemoWindow = true;
-  bool showAnotherWindow = false;
-  ImVec4 clearColor;
-  clearColor.x = 0.45f;
-  clearColor.y = 0.55f;
-  clearColor.z = 0.60f;
-  clearColor.w = 1.00f;
+    #include "CustomFont.c"
+    ImFontAtlas_AddFontDefault(ioptr->Fonts, NULL);
+    const ImWchar icons_ranges[3] = { ICON_IGFD_MIN, ICON_IGFD_MAX, 0 };
+    ImFontConfig* icons_config = ImFontConfig_ImFontConfig();
+    icons_config->MergeMode = true;
+    icons_config->PixelSnapH = true;
+    ImFontAtlas_AddFontFromMemoryCompressedBase85TTF(ioptr->Fonts, FONT_ICON_BUFFER_NAME_IGFD, 15.0f, icons_config, icons_ranges);
+    ImFontConfig_destroy(icons_config);
 
-  bool quit = false;
-  while (!quit)
-  {
-    SDL_Event e;
+    // create ImGuiFileDialog
+    //ImGuiFileDialog* cfiledialog = IGFD_Create();
+    // define some files color
+    //IGFD_SetExtentionInfos2(cfiledialog, ".c", 1.0f, 1.0f, 0.0f, 0.9f, "");
+    //IGFD_SetExtentionInfos2(cfiledialog, ".h", 0.2f, 1.0f, 0.0f, 0.9f, "");
 
-    // we need to call SDL_PollEvent to let window rendered, otherwise
-    // no window will be shown
-    while (SDL_PollEvent(&e) != 0)
+    IGFD_String res = IGFD_String_Get();
+
+    bool showDemoWindow = true;
+    bool showAnotherWindow = false;
+    ImVec4 clearColor;
+    clearColor.x = 0.45f;
+    clearColor.y = 0.55f;
+    clearColor.z = 0.60f;
+    clearColor.w = 1.00f;
+
+    bool quit = false;
+    while (!quit)
     {
-      ImGui_ImplSDL2_ProcessEvent(&e);
-      if (e.type == SDL_QUIT)
-        quit = true;
-      if (e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_CLOSE && e.window.windowID == SDL_GetWindowID(window))
-        quit = true;
-    }
-    
-    // start imgui frame
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplSDL2_NewFrame(window);
-    igNewFrame();
+        SDL_Event e;
 
-    if (showDemoWindow)
-      igShowDemoWindow(&showDemoWindow);
+        // we need to call SDL_PollEvent to let window rendered, otherwise
+        // no window will be shown
+        while (SDL_PollEvent(&e) != 0)
+        {
+            ImGui_ImplSDL2_ProcessEvent(&e);
+            if (e.type == SDL_QUIT)
+                quit = true;
+            if (e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_CLOSE && e.window.windowID == SDL_GetWindowID(window))
+                quit = true;
+        }
 
-    // show a simple window that we created ourselves.
-    {
-      static float f = 0.0f;
-      static int counter = 0;
+        // start imgui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame(window);
+        igNewFrame();
 
-      igBegin("Hello, world!", NULL, 0);
-      igText("This is some useful text");
-      igCheckbox("Demo window", &showDemoWindow);
-      igCheckbox("Another window", &showAnotherWindow);
+        if (showDemoWindow)
+            igShowDemoWindow(&showDemoWindow);
 
-      igSliderFloat("Float", &f, 0.0f, 1.0f, "%.3f", 0);
-      igColorEdit3("clear color", (float*)&clearColor, 0);
+        // show a simple window that we created ourselves.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
 
-      ImVec2 buttonSize;
-      buttonSize.x = 0;
-      buttonSize.y = 0;
-      if (igButton("Button", buttonSize))
-        counter++;
-      igSameLine(0.0f, -1.0f);
-      igText("counter = %d", counter);
+            ImVec2 buttonSize;
+            buttonSize.x = 0;
+            buttonSize.y = 0;
+            
+            igBegin("Hello, world!", NULL, 0);
+            igText("This is some useful text");
+            igCheckbox("Demo window", &showDemoWindow);
+            igCheckbox("Another window", &showAnotherWindow);
 
-      igText("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / igGetIO()->Framerate, igGetIO()->Framerate);
-      igEnd();
-    }
+            igSliderFloat("Float", &f, 0.0f, 1.0f, "%.3f", 0);
+            igColorEdit3("clear color", (float*)&clearColor, 0);
 
-    if (showAnotherWindow)
-    {
-      igBegin("imgui Another Window", &showAnotherWindow, 0);
-      igText("Hello from imgui");
-      ImVec2 buttonSize;
-      buttonSize.x = 0; buttonSize.y = 0;
-      if (igButton("Close me", buttonSize))
-      {
-        showAnotherWindow = false;
-      }
-      igEnd();
-    }
+            if (igButton("Open File", buttonSize))
+            {
+               /* IGFD_OpenDialog(cfiledialog, 
+                    "filedlg",                              // dialog key (make it possible to have different treatment reagrding the dialog key
+                    "Open a File",                          // dialog title
+                    "c files(*.c/*.h){.c,.h}",              // dialog filter syntax : simple => .h,.c,.pp, etc and collections : text1{filter0,filter1,filter2}, text2{filter0,filter1,filter2}, etc..
+                    ".",                                    // base directory for files scan
+                    "",                                     // base filename
+                    0,                                      // a fucntion for display a right pane if you want
+                    0.0f,                                   // base width of the pane
+                    0,                                      // count selection : 0 infinite, 1 one file (default), n (n files)
+                    "User data !",                          // some user datas
+                    ImGuiFileDialogFlags_ConfirmOverwrite); // ImGuiFileDialogFlags*/
+            }
 
-    // render
-    igRender();
-    SDL_GL_MakeCurrent(window, gl_context);
-    glViewport(0, 0, (int)ioptr->DisplaySize.x, (int)ioptr->DisplaySize.y);
-    glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
-    glClear(GL_COLOR_BUFFER_BIT);
-    ImGui_ImplOpenGL3_RenderDrawData(igGetDrawData());
+            if (igButton("Button", buttonSize))
+                counter++;
+            igSameLine(0.0f, -1.0f);
+            igText("counter = %d", counter);
+
+            igText("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / igGetIO()->Framerate, igGetIO()->Framerate);
+            igEnd();
+
+            // display of file dialog
+            ImVec2 maxSize;
+            maxSize.x = ioptr->DisplaySize.x * 0.8f;
+            maxSize.y = ioptr->DisplaySize.y * 0.8f;
+            ImVec2 minSize;
+            minSize.x = maxSize.x * 0.25f;
+            minSize.y = maxSize.y * 0.25f;
+            
+            // display dialog
+            /*if (IGFD_DisplayDialog(cfiledialog, "filedlg", ImGuiWindowFlags_NoCollapse, minSize, maxSize))
+            {
+                if (IGFD_IsOk(cfiledialog)) // result ok
+                {
+                    struct IGFD_String cfilePathName = IGFD_GetFilePathName(cfiledialog);
+                    printf("GetFilePathName : %s\n", cfilePathName.buffer);
+                    struct IGFD_String cfilePath = IGFD_GetCurrentPath(cfiledialog);
+                    printf("GetCurrentPath : %s\n", cfilePath.buffer);
+                    struct IGFD_String cfilter = IGFD_GetCurrentFilter(cfiledialog);
+                    printf("GetCurrentFilter : %s\n", cfilter.buffer);
+                    // here convert from string because a string was passed as a userDatas, but it can be what you want
+                    void* cdatas = IGFD_GetUserDatas(cfiledialog);
+                    if (cdatas)
+                        printf("GetUserDatas : %s\n", (const char*)cdatas);
+
+                    struct IGFD_Selection csel = IGFD_GetSelection(cfiledialog); // multi selection
+                    printf("Selection :\n");
+                    for (int i = 0; i < (int)csel.count; i++)
+                    {
+                        printf("(%i) FileName %s => path %s\n", i, csel.table[i].fileName.buffer, csel.table[i].filePathName.buffer);
+                    }
+                    // action
+
+                    // destroy
+                    IGFD_String_DestroyContent(&cfilePathName);
+                    IGFD_String_DestroyContent(&cfilePath);
+                    IGFD_String_DestroyContent(&cfilter);
+                    IGFD_Selection_DestroyContent(&csel);
+                }
+                IGFD_CloseDialog(cfiledialog);
+            }*/
+        }
+
+        if (showAnotherWindow)
+        {
+            igBegin("imgui Another Window", &showAnotherWindow, 0);
+            igText("Hello from imgui");
+            ImVec2 buttonSize;
+            buttonSize.x = 0; buttonSize.y = 0;
+            if (igButton("Close me", buttonSize))
+            {
+                showAnotherWindow = false;
+            }
+            igEnd();
+        }
+
+        // render
+        igRender();
+        SDL_GL_MakeCurrent(window, gl_context);
+        glViewport(0, 0, (int)ioptr->DisplaySize.x, (int)ioptr->DisplaySize.y);
+        glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(igGetDrawData());
 #ifdef IMGUI_HAS_DOCK
-	if (ioptr->ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        if (ioptr->ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
         {
             SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
             SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
             igUpdatePlatformWindows();
-            igRenderPlatformWindowsDefault(NULL,NULL);
+            igRenderPlatformWindowsDefault(NULL, NULL);
             SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
         }
 #endif
-    SDL_GL_SwapWindow(window);
-  }
+        SDL_GL_SwapWindow(window);
+    }
 
-  // clean up
-  ImGui_ImplOpenGL3_Shutdown();
-  ImGui_ImplSDL2_Shutdown();
-  igDestroyContext(NULL);
+    // destroy ImGuiFileDialog
+    //IGFD_Destroy(cfiledialog);
 
-  SDL_GL_DeleteContext(gl_context);
-  if (window != NULL)
-  {
-    SDL_DestroyWindow(window);
-    window = NULL;
-  }
-  SDL_Quit();
+    // clean up
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    igDestroyContext(NULL);
 
-  return 0;
+    SDL_GL_DeleteContext(gl_context);
+    if (window != NULL)
+    {
+        SDL_DestroyWindow(window);
+        window = NULL;
+    }
+    SDL_Quit();
+
+    return 0;
 }
